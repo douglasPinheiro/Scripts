b9855f4b6dcdebf2e5b4f9441c8b00ff96516920
Douglas <douglas-nba2999@hotmail.com>
2015-07-16 22:54:38 -0300

    Initial with postgres script

---
+++ scripts/postgreSQL.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Installing postgreSQL"  # To get the latest package lists
+sudo apt-get update && sudo apt-get install postgresql postgresql-contrib
+sudo su - postgres 
+psql -d postgres -U postgres # Login with postgres
+
+alter user postgres with password 'postgres';
+\q
+exit
+
+echo "Installing pgadmin3"
+sudo apt-get install pgadmin3 # Install Pgadmin3
